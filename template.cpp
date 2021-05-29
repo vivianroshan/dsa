fed33c05d732385f92ede4b9e538935a329c546d
vivianroshan <78131335+vivianroshan@users.noreply.github.com>
2021-05-29 23:47:49 +0530

    added template

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include<bits\stdc++.h>
+
+#define F first
+#define S second
+#define mp make_pair
+#define vi vector<int>
+#define vli vector<long>
+#define pb push_back
+#define endl '\n'
+#define um unordered_map
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define FORd(i, a, b) for(int i = a-1; i >= b; i--)
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
