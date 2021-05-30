d1e197e53c28e13ccc05b602559817bed6d75a01
vivianroshan <78131335+vivianroshan@users.noreply.github.com>
2021-05-30 12:25:55 +0530

    world record working added

--- World_Record.cpp
+++ World_Record.cpp
@@ -1,4 +1,4 @@
-#include<bits\stdc++.h>
+#include<bits/stdc++.h>
 #include<math.h>
 
 #define F first
@@ -13,25 +13,27 @@
 #define FORd(i, a, b) for(int i = a-1; i >= b; i--)
 #define TIME 9.58
 
+float wr_time = TIME;
+
 using namespace std;
 
 void worldRecord(float v){
-    float wrspeed=round(10000/TIME);
-    wrspeed/=100;
-    v/=100;
-    cout << wrspeed << " " << v <<endl;
-    cout << ((wrspeed>=v)?"NO":"YES") << endl;
+    float time=int(10000/v+0.5);
+    time/=100;
+    cout << wr_time << " " << time <<endl;
+    cout << ((time<wr_time)?"YES":"NO") << endl;
 }
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    
     int n;
     float k1, k2, k3, v;
     cin >> n;
     FOR(i, 0, n){
         cin >> k1 >> k2 >> k3 >> v;
-        worldRecord(round(k1 * k2 * k3 * v * 100));
+        worldRecord(k1 * k2 * k3 * v);
     }
 
     return 0;
