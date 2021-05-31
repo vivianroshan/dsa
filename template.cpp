6a53e96f598f944dfefeaab2dec99815af664735
vivianroshan <78131335+vivianroshan@users.noreply.github.com>
2021-05-31 23:57:25 +0530

    template added with test case loop

--- template.cpp
+++ template.cpp
@@ -5,6 +5,7 @@
 #define mp make_pair
 #define vi vector<int>
 #define vli vector<long>
+#define vll vector<long long>
 #define pb push_back
 #define endl '\n'
 #define um unordered_map
@@ -12,6 +13,8 @@
 #define FORd(i, a, b) for(int i = a-1; i >= b; i--)
 
 using namespace std;
+
+typedef long long ll;
 
 bool sortbysec(const pair<int,int> &a,
               const pair<int,int> &b)
@@ -22,6 +25,11 @@ bool sortbysec(const pair<int,int> &a,
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
+    // cin >> T;
+    // FOR(i, 0, T){
+
+    // }
 
     return 0;
 }
