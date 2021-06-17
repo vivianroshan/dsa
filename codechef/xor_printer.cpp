a45d74941f12b8324330af7a7b38ad92e2c4ce06
vivianroshan <78131335+vivianroshan@users.noreply.github.com>
2021-06-17 14:39:01 +0530

    xor printer formatted

--- codechef/xor_printer.cpp
+++ codechef/xor_printer.cpp
@@ -1,22 +1,30 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
-void M31_31(){
-    for(int i=0; i<=31; i++){
-        for(int j=0; j<=31; j++){
-            cout << (i^j) << "\t";
+void M31_31()
+{
+    for (int i = 0; i <= 31; i++)
+    {
+        for (int j = 0; j <= 31; j++)
+        {
+            cout << (i ^ j) << "\t";
         }
-        cout << "\n";   
+        cout << "\n";
     }
 }
-void M31(){
-    int Xor=0;
-    for(int i=0; i<=100; i++){
+void M31()
+{
+    int Xor = 0;
+    for (int i = 0; i <= 100; i++)
+    {
         // if((i+1)%4==0)cout << i << "\t" << Xor << "\n";
-        Xor=Xor^i;
-        if((i+1)%4==0)cout << i << "\t" << Xor << "\n";
+        Xor = Xor ^ i;
+        if ((i + 1) % 4 == 0)
+            cout << i << "\t" << Xor << "\n";
     }
 }
-int main(){
+int main()
+{
     M31();
+    return 0;
 }
